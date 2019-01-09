6e19b0a52de56e9a702b3279be76fe06539bec16
mad <maddog@mdui-MacBook-Pro.local>
2019-01-09 14:15:36 +0900

    Add sub method and test

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,4 +1,7 @@
 public class Calc {i
   #comment change
-  public int add(int x, int y ) { retunr x +y }
+  public int add(int x, int y ) { retunn x +y; }
+
+  public int sub( int x, int y) { return x-y; }
+
 }
--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -3,8 +3,16 @@ public class CalcTest {
 public testAdd(){
 
  Calc c = new Calc();
- assertEquals( 30. c.add(10, 20));
+ assertEquals( 30, c.add(10, 20));
 
 }
+
+@Test 
+public testSub(){
+
+Calc c = new Calc();
+assertEquals( 10, c.sub(30,20));
+}
+
 
 }
