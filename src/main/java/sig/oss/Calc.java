07bd2ca05c590b9309cff76a6b760cfaca45a860
rabiniz <rabiniz@naver.com>
2019-01-09 17:05:07 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,7 +1,9 @@
-public class Calc {i
+public class Calc {
+  public int r=0;
   #comment change
   public int add(int x, int y ) { retunn x +y; }
 
   public int sub( int x, int y) { return x-y; }
 
+  public void dec(int d) { r-=d);
 }
