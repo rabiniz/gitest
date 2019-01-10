3a882ac6ebabf37043872ac3c23535830bc98911
mad <maddog@mdui-MacBook-Pro.local>
2019-01-10 09:20:20 +0900

    inc func and Test

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,7 +1,11 @@
 public class Calc {i
+  private int r=0;
   #comment change
   public int add(int x, int y ) { retunn x +y; }
 
   public int sub( int x, int y) { return x-y; }
 
+  public int inc( int d) { r+=1;}
+
+  public int getResult(){ return r; }
 }
--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -15,4 +15,12 @@ assertEquals( 10, c.sub(30,20));
 }
 
 
+@Test
+public void testMultipleInc(){
+ Calc c = new Calc();
+ c.inc(25);
+ c.inc(25);
+ assertEquals( 50, c.getResult());
+}
+
 }
